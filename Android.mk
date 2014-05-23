8cf9465c4b456555fde75f6f33a064a439823031
Torne (Richard Coles) <torne@google.com>
2014-05-23 18:08:23 +0100

    Update v8 dependencies.
    The v8_base target no longer has the architecture name in the target
    name, making this dependency much simpler. We also no longer need to
    exclude arm64, since chromium_org now builds for arm64.
    
    Change-Id: I0017655832179cb2b6cd59bc1baffc7a983bc6c0

--- Android.mk
+++ Android.mk
@@ -20,17 +20,10 @@ LOCAL_CFLAGS += \
 
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/src external/chromium_org/v8
 
-# Temporarily disable arm64 as WebView isn't being built there yet.
-LOCAL_MODULE_UNSUPPORTED_TARGET_ARCH := arm64
-
 # Depend on V8 from WebView
 # DO NOT COPY without permission from WebView Owners
-LOCAL_STATIC_LIBRARIES_x86_64 := v8_tools_gyp_v8_base_x64_gyp
-LOCAL_STATIC_LIBRARIES_x86 := v8_tools_gyp_v8_base_ia32_gyp
-LOCAL_STATIC_LIBRARIES_arm64 := v8_tools_gyp_v8_base_arm64_gyp
-LOCAL_STATIC_LIBRARIES_arm := v8_tools_gyp_v8_base_arm_gyp
-LOCAL_STATIC_LIBRARIES_mips := v8_tools_gyp_v8_base_mipsel_gyp
-LOCAL_STATIC_LIBRARIES := v8_tools_gyp_v8_snapshot_gyp
+LOCAL_STATIC_LIBRARIES := v8_tools_gyp_v8_base_gyp \
+                          v8_tools_gyp_v8_snapshot_gyp
 
 LOCAL_SHARED_LIBRARIES := libutils libstlport liblog libicui18n libicuuc
 
